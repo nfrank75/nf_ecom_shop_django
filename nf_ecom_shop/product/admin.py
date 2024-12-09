4d3d427305a2df8f2328045b520ca2458c7c46ed
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-12-09 12:10:15 +0100

    Get all products & Product Serializer

--- nf_ecom_shop/product/admin.py
+++ nf_ecom_shop/product/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
+admin.site.site_header = "E-Commerce Rest API"
+admin.site.site_title = "E-commerce Rest API"
+admin.site.index_title = "E-commerce Rest API"
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
--- nf_ecom_shop/product/views.py
+++ nf_ecom_shop/product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import ProductSerializer
+from .models import Product
+
+
+@api_view(['GET'])
+def get_products(request):
+
+    products = Product.objects.all()
+    
+    serializer = ProductSerializer(products, many=True)
+
+    return Response({
+        'products': serializer.data
+        })
