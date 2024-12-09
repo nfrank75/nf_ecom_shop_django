2fee82855388e021c61873c4053212d0b6bbd1df
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-12-09 17:07:05 +0100

    Adding Product Search like keywork, min_price, max_price

--- nf_ecom_shop/product/filters.py
+++ nf_ecom_shop/product/filters.py
@@ -3,6 +3,13 @@ from django_filters import rest_framework as filters
 from .models import Product
 
 class ProductsFilter(filters.FilterSet):
+
+    keyword = filters.CharFilter(field_name="name", lookup_expr="icontains")
+
+    min_price = filters.NumberFilter(field_name="price" or 0, lookup_expr="gte")
+
+    max_price = filters.NumberFilter(field_name="price" or 10000000, lookup_expr="lte")
+
     class Meta:
         model = Product
-        fields = ('category', 'brand')
+        fields = ('category', 'brand', 'keyword', 'min_price', 'max_price')
