9f6141504ccba26326de1a13c654710498de4ccd
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-12-13 16:25:54 +0100

    Add Handle 404 & Internal Server Error 500

--- nf_ecom_shop/product/models.py
+++ nf_ecom_shop/product/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.TextChoices):
+    PHONES = 'TELEPHONES'
     ELECTRONICS = 'Electronics'
     LAPTOPS = 'Latops'
     ARTS = 'Arts'
--- nf_ecom_shop/product/views.py
+++ nf_ecom_shop/product/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
 
 from .serializers import ProductSerializer
 from .models import Product
@@ -12,15 +14,28 @@ from .filters import ProductsFilter
 def get_products(request):
 
     filterset = ProductsFilter(request.GET, queryset=Product.objects.all().order_by('id'))
+
+    number_of_product = filterset.qs.count()
+
+    # Pagination
+    resPerPage = 5
+
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
     
-    serializer = ProductSerializer(filterset.qs, many=True)
+    serializer = ProductSerializer(queryset, many=True)
 
     return Response({
+        'number_of_product': number_of_product,
+        'resPerPage':resPerPage,
         'products': serializer.data
         })
 
 @api_view(['GET'])
-def get_product(request, pk):
+def get_product(request, pkk):
 
     product = get_object_or_404(Product, id=pk)
     
