082f2f56a38b4d26bff29409720003707c32eda7
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-12-20 12:55:43 +0100

    Add Account application

--- nf_ecom_shop/nf_ecom_shop/settings.py
+++ nf_ecom_shop/nf_ecom_shop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     
     'product',
+    'account',
 ]
 
 MIDDLEWARE = [
