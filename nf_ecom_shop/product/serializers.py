af2daca94a979031c96bee3ae485716f2c54bf36
Frank NGALEU <frank.ngaleu@its-nh.com>
2024-12-20 09:22:14 +0100

    Display products on ProductSerializer

--- nf_ecom_shop/product/serializers.py
+++ nf_ecom_shop/product/serializers.py
@@ -2,15 +2,19 @@ from rest_framework import serializers
 from .models import Product, ProductImages
 
 
-class ProductSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Product
-        fields = "__all__"
-
     
 class ProductImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductImages
         fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    images = ProductImagesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'brand', 'category', 'stock', 'user', 'images')
+
